6fca9b9b1c78e44204b5dba1d80f30cbcc4b3dff
jaspreet kaur <jaspreetkaur16june@gmail.com>
2021-10-29 00:19:39 +0530

    Updated

--- src/row.jsx
+++ src/row.jsx
@@ -1,25 +1,84 @@
-import React from 'react'
-import { useState ,useEffect} from 'react'
-import axios from "./axios"
+// import React from "react";
+// import { useState, useEffect } from "react";
+// import axios from "./axios";
 
-function Row({title,fetchURL}) {
+// const baseURL = "https://image.tmdb.org/t/p/original";
 
-    let [movies, setMovies] = useState([]);
+// function Row({ title, fetchURL }) {
+//   let [movies, setMovies] = useState([]);
 
-    useEffect(() => {
-        async function fetch(){
-            const request = await axios.get(fetchURL);
-            console.log(request);
-            return request;
-        }
-        fetch();
-    }, [])
+//   useEffect(() => {
+//     async function fetch() {
+//       const request = await axios.get(fetchURL);
+//       setMovies(request.data.result);
+//       console.log(request);
+//       return request;
+//     }
+//     fetch();
+//   }, [fetchURL]);
 
-    return (
-        <div>
-         <h2>{title}</h2>   
-        </div>
-    )
+//   console.log(movies);
+
+//   return (
+//     <div className="row">
+//       <h2>{title}</h2>
+//       <div className="row-posters">
+//         {/* {movies.map((movie)=>(
+//                  <img className = "row-poster" src={`${baseURL}${movie.poster_path}`} alt= {movie.name}/>
+//              ))} */}
+//         {movies.map((movie) => (
+//           <img
+//             key={movie.id}
+//             className="row_poster"
+//             src={`${baseURL}${movie.poster_path}`}
+//             alt={movie.name}
+//           />
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Row;
+
+
+
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+// import "./css/Row.css";
+
+const base_url = "https://image.tmdb.org/t/p/original";
+
+function Row({ title, fetchURL }) {
+  let [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchMovies() {
+      const request = await axios.get(fetchURL);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchMovies();
+  }, [fetchURL]);
+
+  console.log(movies);
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+
+      <div className="row_posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className="row_poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Row;
