6da0ffcdbfde3ed32f826eeaa1938c0ba34ba20e
jaspreet kaur <jaspreetkaur16june@gmail.com>
2021-10-29 00:49:29 +0530

    row Css updated

--- src/row.jsx
+++ src/row.jsx
@@ -1,52 +1,6 @@
-// import React from "react";
-// import { useState, useEffect } from "react";
-// import axios from "./axios";
-
-// const baseURL = "https://image.tmdb.org/t/p/original";
-
-// function Row({ title, fetchURL }) {
-//   let [movies, setMovies] = useState([]);
-
-//   useEffect(() => {
-//     async function fetch() {
-//       const request = await axios.get(fetchURL);
-//       setMovies(request.data.result);
-//       console.log(request);
-//       return request;
-//     }
-//     fetch();
-//   }, [fetchURL]);
-
-//   console.log(movies);
-
-//   return (
-//     <div className="row">
-//       <h2>{title}</h2>
-//       <div className="row-posters">
-//         {/* {movies.map((movie)=>(
-//                  <img className = "row-poster" src={`${baseURL}${movie.poster_path}`} alt= {movie.name}/>
-//              ))} */}
-//         {movies.map((movie) => (
-//           <img
-//             key={movie.id}
-//             className="row_poster"
-//             src={`${baseURL}${movie.poster_path}`}
-//             alt={movie.name}
-//           />
-//         ))}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Row;
-
-
-
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
-// import "./css/Row.css";
-
+import "./css/row.css";
 const base_url = "https://image.tmdb.org/t/p/original";
 
 function Row({ title, fetchURL }) {
